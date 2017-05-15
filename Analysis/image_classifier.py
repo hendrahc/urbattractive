5d29018c1873fd2ec8faede18e2f0856c7073b3f
hendrahc <hendra.h2c.x@gmail.com>
2017-05-15 17:03:28 +0200

    update complete dataset

--- Analysis/image_classifier.py
+++ Analysis/image_classifier.py
@@ -12,6 +12,9 @@ import numpy as np
 import warnings
 import pandas as pd
 
+from keras import metrics
+from keras import optimizers
+
 from keras.models import Model, Sequential
 from keras.layers import Flatten, Dropout
 from keras.layers import Dense
@@ -74,7 +77,7 @@ def create_basic_model():
     # Classification block
     x = Flatten(name='flatten')(x)
     x = Dense(4096, activation='relu', name='fc6', trainable=False)(x)
-    x = Dense(4096, activation='relu', name='fc7')(x)
+    x = Dense(4096, activation='relu', name='fc7', trainable=True)(x)
     x = Dense(205, activation='softmax', name='fc8')(x)
 
     inputs = img_input
@@ -83,94 +86,27 @@ def create_basic_model():
     model = Model(inputs, x, name='vgg16')
     return model
 
-def create_basic_model2():
-    input_shape = (3,224, 224)
-    model = Sequential()
-
-    model.add(ZeroPadding2D((1, 1), input_shape=input_shape))
-    model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_1'))
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_2'))
-    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_1'))
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_2'))
-    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_1'))
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_2'))
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_3'))
-    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_1'))
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_2'))
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_3'))
-    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_1'))
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_2'))
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_3'))
-    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-    model.add(Flatten(name="flatten"))
-    model.add(Dense(4096, activation='relu', name='fc6'))
-    model.add(Dropout(0.5))
-    model.add(Dense(4096, activation='relu', name='fc7'))
-    model.add(Dropout(0.5))
-    model.add(Dense(205, name='fc8'))
-
-    return model
-
-
-def load_VGG_weight(model,weights_path):
-    # load weights
-    model.load_weights(weights_path)
-    if K.backend() == 'theano':
-        layer_utils.convert_all_kernels_in_model(model)
-
-    if K.image_data_format() == 'channels_first':
-        maxpool = model.get_layer(name='block5_pool')
-        shape = maxpool.output_shape[1:]
-        dense = model.get_layer(name='fc1')
-        layer_utils.convert_dense_weights_data_format(dense, shape, 'channels_first')
-
-        if K.backend() == 'tensorflow':
-            warnings.warn('You are using the TensorFlow backend, yet you '
-                          'are using the Theano '
-                          'image data format convention '
-                          '(`image_data_format="channels_first"`). '
-                          'For best performance, set '
-                          '`image_data_format="channels_last"` in '
-                          'your Keras config '
-                          'at ~/.keras/keras.json.')
-    return model
-
 def load_PLACES_weight(model,weights_path):
     model.load_weights(weights_path)
     return model
 
-def predict_VGG(model,img_path):
-    x = read_img(img_path)
-    x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
-    preds = model.predict(x)
-    return preds
+def preprocess_image(x):
+    x[0], x[1], x[2] = x[2].transpose() - 105.487823486, x[1].transpose() - 113.741088867, x[0].transpose() - 116.060394287
+    return x
 
-def save_model(model,out_file,weight_file,layer_name):
+def preprocess_dataset(dat):
+    n = dat.shape[0]
+    for i in range(0,n):
+        x = dat[i]
+        x[0], x[1], x[2] = x[2].transpose() - 105.487823486, x[1].transpose() - 113.741088867, x[0].transpose() - 116.060394287
+        dat[i] = x
+    return dat
+
+def save_model(model,out_file,weight_file,layer_name=""):
     model_json = model.to_json()
     with open(out_file, "w") as json_file:
         json_file.write(model_json)
+    json_file.close()
     print("model saved")
     if(weight_file):
         model.save_weights(weight_file)
@@ -186,10 +122,6 @@ def load_model(in_file,weight_file):
     if (weight_file):
         loaded_model.load_weights(weight_file)
     print("weights loaded")
-
-    loaded_model.compile(loss='binary_crossentropy',
-                  optimizer='rmsprop',
-                  metrics=['accuracy'])
 
     return loaded_model
 
@@ -208,6 +140,7 @@ def load_dataset(path,ref_file):
         x = read_img(path+filename)
         cls = row["median"]
         y = []
+
         if cls==1:
             y = [0, 0, 0, 0]
         elif cls == 2:
@@ -219,28 +152,34 @@ def load_dataset(path,ref_file):
         elif cls == 5:
             y = [1, 1, 1, 1]
         else:
+            y = [0, 0, 0, 0]
+
+        '''
+        if cls == 1:
+            y = [1, 0, 0, 0,0]
+        elif cls == 2:
+            y = [0, 1, 0, 0, 0]
+        elif cls == 3:
+            y = [0, 0, 1, 0, 0]
+        elif cls == 4:
+            y = [0, 0, 0, 1, 0]
+        elif cls == 5:
+            y = [0, 0, 0, 0, 1]
+        else:
             continue
+        '''
+
         X.append(x)
         Y.append(y)
     X = np.array(X)
     Y = np.array(Y)
-
-    #split for training and validation
-    n = X.shape[0]
-    #forval = [i for i in range(1,n) if i%10==4]
-    forval = [i for i in range(0,56)]
-    fortrain = [i for i in range(0,n) if i not in forval]
-    X_train = X[fortrain]
-    Y_train = Y[fortrain]
-    X_val = X[forval]
-    Y_val = Y[forval]
-
-    return [X_train,Y_train,X_val,Y_val]
+    return [X,Y]
 
 def predict_attractiveness(model,img_path):
     x = read_img(img_path)
+    x = preprocess_image(x)
+    x = np.expand_dims(x, axis=0)
     preds = model.predict(x)
-    print(preds)
     return preds
 
 
@@ -251,27 +190,26 @@ def class_accuracy(y_true,y_pred):
 def train_model(model,X_train,Y_train,X_val,Y_val):
     # dimensions of our images.
     img_width, img_height = 224, 224
-    nb_train_samples = 565
-    nb_validation_samples = 565
-    epochs = 20
-    batch_size = 8
+    nb_train_samples = X_train.shape[0]
+    nb_validation_samples = X_val.shape[0]
+    epochs = 100
+    batch_size = 1
 
-    optim = keras.optimizers.SGD(lr=0.1, momentum=0.0, decay=0.99, nesterov=False);
+    optim = keras.optimizers.SGD(lr=10, momentum=0.0, decay=0.8, nesterov=False);
 
     model.compile(loss='binary_crossentropy',
                   optimizer=optim,
-                  metrics=[class_accuracy])
+                  metrics=['accuracy',metrics.categorical_accuracy])
 
     # this is the augmentation configuration we will use for training
     train_datagen = ImageDataGenerator(
-        rescale=1. / 255,
         shear_range=0.2,
         zoom_range=0.2,
         horizontal_flip=True)
 
     # this is the augmentation configuration we will use for testing:
     # only rescaling
-    test_datagen = ImageDataGenerator(rescale=1. / 255)
+    test_datagen = ImageDataGenerator()
 
     train_generator = train_datagen.flow(
         X_train, Y_train,
@@ -292,35 +230,30 @@ def train_model(model,X_train,Y_train,X_val,Y_val):
     return model
 
 def start_model():
-    #WEIGHTS_PATH = '../../CNN/PredefinedModels/vgg16_weights_tf_dim_ordering_tf_kernels.h5'
-    #WEIGHTS_PATH_NO_TOP = '../../CNN/PredefinedModels/vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5'
-
-    WEIGHTS_PATH = '../../CNN/PredefinedModels/vgg_places.h5'
+    WEIGHTS_PATH = '../../CNN/PredefinedModels/vgg_places_keras.h5'
     model = create_basic_model()
-    model = load_VGG_weight(model,WEIGHTS_PATH)
+    model = load_PLACES_weight(model, WEIGHTS_PATH)
 
-    last = model.layers[-2].output
-    x = Dense(4096, activation='relu', name='fc2new', trainable=True)(last)
-    x = Dense(4, activation='softmax', name='predictor', trainable=True)(x)
+    last = model.layers[-1].output
+    #x = Dense(4096, activation='relu', name='fc7new', trainable=True)(last)
+    x = Dense(4, activation='sigmoid', name='predictor', trainable=True)(last)
     model = Model(model.input, x, name='newModel')
-
-    #plot_model(model, to_file='CNNModels/model.png')
-
-    #model_file = "CNNModels/model.json"
-    #weight_file = "CNNModels/weight.h5"
-    #save_model(model,model_file,weight_file)
     return model
 
 def testModel(model,X_val,Y_val):
-    preds = model.predict(X_val,batch_size=1)
-    preds = preds.astype(int)
-    n = preds.shape[0]
+    preds = model.predict(X_val,batch_size=10)
+    preds = binarize_result(preds)
+    accuracy = get_accuracy(preds,Y_val)
+    print("accuracy = "+str(accuracy))
+    return preds
+
+def get_accuracy(Y_pred,Y_true):
+    n = Y_pred.shape[0]
     correct = 0
     for i in range(0,n):
-        if(np.array_equal(preds[i],Y_val[i])):
+        if(np.array_equal(Y_pred[i],Y_true[i])):
             correct = correct+1
-    return correct/n
-
+    return float(correct)/float(n)
 
 def convert_weight(h5_file = '../../CNN/PredefinedModels/vgg_places.h5',out_file = '../../CNN/PredefinedModels/vgg_places_keras.h5'):
     res = h5py.File(out_file,'r+')
@@ -360,44 +293,147 @@ def decode_scene(preds,reff):
 def classify_scene(model,reff,img_path):
     img = image.load_img(img_path, target_size=(224, 224))
     x = image.img_to_array(img)
-    x[0],x[1],x[2] = x[2].transpose()-105.487823486,x[1].transpose()-113.741088867,x[0].transpose()-116.060394287
+    x = preprocess_image(x)
     x = np.expand_dims(x, axis=0)
     preds = model.predict(x)[0]
     decode_scene(preds,reff)
     return preds
 
-def run_training(name):
+def predict_scenes():
+    path = "../Website/crowdsourcing/public/images/"
+    ref = "CrowdData/pilot_aggregates_part1.csv"
+    dat = pd.read_csv(ref)
+    reff = get_places_ref()
+    [X,Y] = load_dataset(path, ref)
+    X = preprocess_dataset(X)
+
     WEIGHTS_PATH = '../../CNN/PredefinedModels/vgg_places_keras.h5'
     model = create_basic_model()
-    model = load_PLACES_weight(model,WEIGHTS_PATH)
+    model = load_PLACES_weight(model, WEIGHTS_PATH)
 
+    preds = model.predict(X)
+
+    df_scene = pd.DataFrame(
+        columns=["img_id", "scene1", "scene2", "scene3", "scene4", "scene5"])
+
+    for i in range(0,preds.shape[0]):
+        scene = {}
+        pred_i = preds[i]
+        scene["img_id"] = dat["img_id"][i]
+        sorted = np.flip(pred_i.argsort(), 0)
+        for j in range(1, 6):
+            ct = sorted[j-1]
+            scene["scene"+str(j)] = "'"+reff[str(ct)]+"'"
+        df_scene = df_scene.append(scene, ignore_index=True)
+
+        df_scene["img_id"] = df_scene["img_id"].astype(int)
+    df_scene.to_csv("Data/SceneFeatures.csv")
+
+
+
+def experiment():
+    path = "../Website/crowdsourcing/public/images/"
+    ref = "CrowdData/pilot_aggregates_part1.csv"
+    [X,Y] = load_dataset(path, ref)
+    X = preprocess_dataset(X)
+
+    # split for training and validation
+    n = X.shape[0]
+    n_fold = 5
+    fold_size = int(n/n_fold)
+
+    for fold in range(0,n_fold):
+        forval = [i for i in range(fold*fold_size, (fold+1)*fold_size)]
+        fortrain = [i for i in range(0, n) if i not in forval]
+        X_train = X[fortrain]
+        Y_train = Y[fortrain]
+        X_val = X[forval]
+        Y_val = Y[forval]
+
+        model = start_model()
+        model = train_model(model, X_train, Y_train, X_val, Y_val)
+
+def convert_to_binary(pred):
+    res = np.zeros(pred.shape).astype(int)
+    res[pred>0.5] = 1
+    return res
+
+def binarize_result(preds):
+    for i in range(0,preds.shape[0]):
+        preds[i] = convert_to_binary(preds[i])
+    return preds
+
+
+def run_training(name):
     path="../Website/crowdsourcing/public/images/"
     ref="CrowdData/pilot_aggregates_part1.csv"
-    [X_train,Y_train,X_val,Y_val] = load_dataset(path,ref)
+    [X,Y] = load_dataset(path,ref)
+
+    n = X.shape[0]
+    n_fold = 5
+    fold_size = int(n / n_fold -1)
+
+    X = preprocess_dataset(X)
+    fold = 0
+    forval = [i for i in range(fold * fold_size, (fold + 1) * fold_size)]
+    fortrain = [i for i in range(0, n) if i not in forval]
+    X_train = X[fortrain]
+    Y_train = Y[fortrain]
+    X_val = X[forval]
+    Y_val = Y[forval]
 
     model = start_model()
-    model = train_model(model,X_train,Y_train,X_val,Y_val)
+    model = train_model(model, X_train, Y_train, X_val, Y_val)
 
-    save_model(model,"CNNModels/"+name,"CNNModels/"+name)
+    save_model(model,"../../CNN/Models/"+name+".json","../../CNN/Models/"+name+".h5")
 
-def test_prediction():
+def test_scene_prediction():
     WEIGHTS_PATH = '../../CNN/PredefinedModels/vgg_places_keras.h5'
-    model = create_basic_model2()
+    model = create_basic_model()
+
     model = load_PLACES_weight(model, WEIGHTS_PATH)
 
     img_path = '../Website/crowdsourcing/public/images/PILOT/GSV_PILOT_2_2.jpg'
     reff = get_places_ref()
     tes = classify_scene(model,reff,img_path)
+    return model
 
-'''
-img_path = '../Website/crowdsourcing/public/images/PILOT/GSV_PILOT_93_2.jpg'
-preds = predict_VGG(model,img_path)
-print('Predicted:', decode_predictions(preds))
-mymodel = load_model("CNNModels/trial2.json","CNNModels/trial2.h5")
-score = mymodel.evaluate(X_val, Y_val, batch_size=1)
-preds = mymodel.predict(X_val,batch_size=1)
-'''
 
-#run_training("trial1")
+def tesTrial():
+    model = load_model("../../CNN/Models/trial_naif.json", "../../CNN/Models/trial_naif.h5")
+
+    path="../Website/crowdsourcing/public/images/"
+    ref="CrowdData/pilot_aggregates_part1.csv"
+    [X,Y] = load_dataset(path,ref)
+    X = preprocess_dataset(X)
+    X_tes = X[0:10]
+    Y_tes = Y[0:10]
+    preds = model.predict(X_tes, batch_size=10)
+
+    return [model,X,Y,X_tes,Y_tes,preds]
+
+def tes_training_work():
+    path = "../Website/crowdsourcing/public/images/"
+    ref = "CrowdData/pilot_aggregates_part1.csv"
+    [X, Y] = load_dataset(path, ref)
+    X = preprocess_dataset(X)
+
+    X_train = X[0:4]
+    Y_train = Y[0:4]
+    Y_train[0] = np.array([1,1,0,0])
+    Y_train[1] = np.array([0, 1, 1, 0])
+    Y_train[2] = np.array([0, 0, 1, 1])
+    Y_train[3] = np.array([0, 0, 0, 1])
+
+    model = start_model()
+    model = train_model(model, X_train, Y_train, X_train, Y_train)
+
+    save_model(model, "../../CNN/Models/overfit.json", "../../CNN/Models/overfit.h5")
+
+#run_training("trial_naif")
+#tes_training_work()
+
+#[model,X,Y,X_tes,Y_tes,preds] = tesTrial()
 #test_prediction()
+predict_scenes()
 
